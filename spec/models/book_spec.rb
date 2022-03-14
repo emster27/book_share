ebd27ce8926a3ee00715bf19d435ccd6c1a950a3
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-14 12:07:26 +0000

    Validate presence of books.description

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Book, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:description) }
+
     it { should validate_presence_of(:publish_year) }
 
     it { should validate_presence_of(:title) }
