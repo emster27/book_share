847996eacb3367c57a3dec3d549f5ab03ecab85d
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-14 12:08:21 +0000

    Generate graphiti resources for authors

---
+++ app/resources/author_resource.rb
@@ -0,0 +1,13 @@
+class AuthorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :bio, :string
+  attribute :book_count, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
