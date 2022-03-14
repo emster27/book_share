cfb4d6501aa92842ced5de45917502cfb069b05a
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-14 12:07:37 +0000

    Whitelist columns in Book for ActiveAdmin

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Book do
 
+ permit_params :title, :publish_year, :pages, :description, :image, :author_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
