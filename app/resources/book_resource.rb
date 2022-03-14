702e11082ba217ab94ae6e830b3c7586e4a8c8bb
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-03-14 12:08:16 +0000

    Generate graphiti resources for books

---
+++ app/resources/book_resource.rb
@@ -0,0 +1,16 @@
+class BookResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :publish_year, :integer
+  attribute :pages, :integer
+  attribute :description, :string
+  attribute :image, :string
+  attribute :author_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
